8aa7664862a08e43ce6ae679289ab167e6920159
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2021-11-24 12:53:01 +0200

    Added to header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import styled from 'styled-components'
 function Header() {
     return (
-        <div>
+        <Container>
             Header
-        </div>
+        </Container>
     )
 }
 
 export default Header
+
+const Container = styled.div`
+
+`
